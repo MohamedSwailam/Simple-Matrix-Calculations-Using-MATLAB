6adf81fd43b08b0f71c0b9da025bd7f3ebc4538d
MOHAMED SWAILAM <mohamedswailam@gmail.com>
2015-06-29 17:19:48 +0200

    Create simplematrixcalculations.m
    calculations script file

---
+++ simplematrixcalculations.m
@@ -0,0 +1,24 @@
+% Simple Matrix Calculations
+% Author: Mohamed Swailam
+% Date: 29.06.2015 
+
+
+clear all;
+% Let u, v be row-vectors given by:
+% u = (1, 3, −2), v = (0.2, 0.5, 0.3)
+u = [1 ,3 , -2]; 
+v = [0.2 ,0.5 ,0.3];
+
+% Calculate the following (Noting that the operator ' denotes the transpose):
+
+% (a) 3 * u
+3*u
+
+% (b) u + v
+u+v
+
+% (c) u * v'
+u*v'
+
+% (d) u'* v
+u'*v
